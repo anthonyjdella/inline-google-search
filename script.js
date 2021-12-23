98f3b9d03cb44ee6825e1f34dfc8f9e44d07a87d
Anthony_Dellavecchia <leeznon@gmail.com>
2021-12-22 21:15:00 -0600

    feat(manfiest): options and popup

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
-        "title": 'Search Inline Google for "%s"',
-        "contexts": ["selection"],
-        "id": "myContextMenuId"
+        title: 'Search Inline Google for "%s"',
+        contexts: ["selection"],
+        id: "myContextMenuId"
     });
 });
     
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
+
+    // opens new tab
     chrome.tabs.create({  
         url: "http://www.google.com/search?q=" + encodeURIComponent(info.selectionText)
     });
+
+    chrome.action.setBadgeText({
+        text: "OFF"
+    });
+
+    // search for hardcoded text
+    // chrome.search.query({
+    //     disposition: "CURRENT_TAB",
+    //     text: "%s"
+    // });
+
+    // opens inline
 })
